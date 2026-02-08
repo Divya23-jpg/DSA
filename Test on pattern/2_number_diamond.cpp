72dc11eec659ef757abba102873b625a46a9bd51
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-02-08 23:05:22 +0530

    divya

---
+++ Test on pattern/2_number_diamond.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Number::";
+    cin>>n;
+    int m=n+1/2;
+    for(int i=1;i<=m;i++){
+      
+       
+      
+    cout<<endl;
+    }
+
+    return 0;
+
+}
