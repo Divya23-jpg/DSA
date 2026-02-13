fbee3360adfa1bdf88344f4999f214861df246f1
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-02-13 23:19:10 +0530

    leetcode

---
+++ Leetcode/66_plus_one.cpp
@@ -0,0 +1,18 @@
+/*
+Input: digits = [1,2,3]
+Output: [1,2,4]
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector <int> digits={1,2,3};
+    for(int i=0;i<digits.size();i++){
+        cout<<digits[i];
+
+
+    }
+    return 0;
+
+}
