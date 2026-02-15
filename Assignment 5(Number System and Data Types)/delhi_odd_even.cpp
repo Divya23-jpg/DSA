4d8267769bd0e3ddadee4570742348972d1daf98
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-02-15 20:13:57 +0530

    Divya

--- Assignment 5(Number System and Data Types)/delhi_odd_even.cpp
+++ Assignment 5(Number System and Data Types)/delhi_odd_even.cpp
@@ -17,13 +17,39 @@ No
 
 #include<iostream>
 using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        int a,b;
-        cin>>a>>b;
-        
 
-    }
+int sum(int n){
+	int s=0;
+	for(int i=0;i<=sizeof(n);i++){
+		int k=n%10;
+		s=s+k;
+		n=n/10;
+	}
+	return(s);
+}
+
+
+int main() {
+	int n;
+	cin>>n;
+	int su;
+	int arr[n]={};
+	for(int i=1;i<=n;i++){
+		cin>>arr[i];	
+	}
+	for(int i=1;i<=n;i++){	
+		int su=sum(arr[i]);
+	}
+    // cout<<su;
+	for(int i=1;i<=n;i++){	
+		int su=sum(arr[i]);
+		
+		if(su % 3 == 0){
+			cout<<"Yes"<<endl;
+		}
+		else {
+			cout<<"No";
+		}
+	}
+	return 0;
 }
