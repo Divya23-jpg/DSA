4ed3121398c288f01c3d4117ede6038150aa3a1e
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-02-15 18:17:29 +0530

    divya

--- Assignment 5(Number System and Data Types)/find_all_armstrong.cpp
+++ Assignment 5(Number System and Data Types)/find_all_armstrong.cpp
@@ -1,31 +1,45 @@
+/*
+Sample Input
+400
+1000
+Sample Output
+407
+
+Take the following as input.
+
+A number (N1)
+A number (N2)
+Write a function which prints all armstrong numbers between N1 and N2 (inclusive).
+
+371 is an Armstrong number as 371 = 3^3 + 7^3 + 1^3
+
+*/
+
 #include<iostream>
 #include<cmath>
 using namespace std;
 int main(){
-    int n;
-    cin>>n;
-    int og=n;
-    int temp=n;
-    int sum=0,l=0;
+    int n,m;
+    cin>>n>>m;
+    for(int i=n;i<=m;i++){
+        //total digits 
+        int og=i,l=0;
+        while(og!=0){
+            og=og/10;
+            l++;
 
-    //calculate total digit in n
-    while(temp!=0){
-        temp=temp/10;
-        l++;
-    }
-    
-    //find digit and sum
-    while(n!=0){
-        int k=n%10;
-        sum=sum+pow(k,l);
-        n=n/10;
-    }
-    if(og==sum){
-        cout<<"Armstrong number ";
-    }
-    else{
-        cout<<"Not Armstrong number ";
+        }
+        // pow to every digits by total digits and sum
+        int temp=i;
+        int sum=0;
+        while(temp!=0){
+            int k=temp%10;
+            sum=sum+pow(k,l);
+            temp=temp/10;   
+        }
+        if(i==sum){
+            cout<<i<<" ";
+        }
     }
     return 0;
-
 }
