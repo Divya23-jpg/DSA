de35e7466114e8e0180195143c8cd90f060f05e0
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-02-18 14:37:22 +0530

    divya

--- Lecture 20/selection_sort.cpp
+++ Lecture 20/selection_sort.cpp
@@ -7,8 +7,22 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
+
     
 //selection sort
+for(int i=0;i<n;i++){
+    int min=i;
+    for(int j=i+1;j<n ;j++){
+        if(arr[j]<arr[min]){
+            min=j;
+
+        }
+        swap(arr[i],arr[min]);
+
+
+    }
+
+}
 
 
     for(int i=0;i<n;i++){
