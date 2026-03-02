8c8a165c77be8db9c570c0df9a16ffbb013203e3
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-03 00:26:29 +0530

    divya

--- Assignment 6 Arrays/bubble_sort.cpp
+++ Assignment 6 Arrays/bubble_sort.cpp
@@ -14,10 +14,9 @@ output
 */
 
 
+
 #include<iostream>
 using namespace std;
-#include<algorithm>
-// #include<cmath>
 int main(){
     int n;
     cin>>n;
@@ -26,19 +25,22 @@ int main(){
         cin>>arr[i];
     }
 
+//bubble sort
     for(int i=0;i<n-1;i++){
-        for(int j=0;i<n-i-1;j++){
+        for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
+                // int temp=arr[i];
+                // arr[i]=arr[j];
+                // arr[j]=temp;
                 swap(arr[j],arr[j+1]);
+
+
             }
         }
     }
     for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+        cout<<arr[i]<<'\n';
     }
-
     return 0;
-
-
 
 }
