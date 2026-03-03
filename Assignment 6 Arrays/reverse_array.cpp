dcd11da063b12a4844daba9473c1e8e8a318d170
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-04 01:05:56 +0530

    divya

--- Assignment 6 Arrays/reverse_array.cpp
+++ Assignment 6 Arrays/reverse_array.cpp
@@ -7,9 +7,7 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
-    // for(int i=0;i<n;i++){
-    //     cout<<a[i];
-    // }
+  
     for(int i=n;i>=0;i--){
         cout<<a[i];
     }
