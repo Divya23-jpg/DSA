8d6a980427a36d54d34a4672dfa3d7332ab7de37
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-05 22:53:00 +0530

    divya

---
+++ Assignment 6 Arrays/sorted_array_square.cpp
@@ -0,0 +1,19 @@
+/*
+Sample Input
+5
+-4 -1 0 3 10
+
+Sample Output
+0 1 9 16 100
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    
+}
