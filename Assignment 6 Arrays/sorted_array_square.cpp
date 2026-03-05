6d29efeee3f2b7ef0a54e0d03a1ba783cbff3b58
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-05 23:16:40 +0530

    divya

--- Assignment 6 Arrays/sorted_array_square.cpp
+++ Assignment 6 Arrays/sorted_array_square.cpp
@@ -15,5 +15,24 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
-    
+    int p[n];
+    for(int i=0;i<n;i++){
+        p[i]=a[i]*a[i];
+       
+        
+    }
+    for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(p[j]>p[j+1]){
+                    swap(p[j],p[j+1]);
+                }
+            }
+          
+        }
+    for(int i=0;i<n;i++){
+        cout<<p[i]<<" ";
+    }
+   
+   
+    return 0;
 }
