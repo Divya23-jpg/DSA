20ec845594f4d2ede1cad1776f5aa73f2d5b3102
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-09 14:41:54 +0530

    divya

--- Leetcode/1572_diagonal_sum.cpp
+++ Leetcode/1572_diagonal_sum.cpp
@@ -14,6 +14,9 @@ int diagonalSum(vector<vector<int>>& mat) {
         return sum;
     }
 int main(){
-   
+   vector<vector<int>> mat={[1,2,3],
+              [4,5,6],
+              [7,8,9]}
+       diagonalSum(mat);
     
 }
