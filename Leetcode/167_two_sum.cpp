5b99e918900fa126c1bded95fddfc4d55751efe0
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-11 23:42:40 +0530

    divya

--- Leetcode/167_two_sum.cpp
+++ Leetcode/167_two_sum.cpp
@@ -12,17 +12,29 @@ Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We retu
 using namespace std; 
 int main(){
     vector <int> numbers={2,7,11,15,18};
+    vector <int> ans;
     int n=numbers.size();
 
+    // for(int i=0;i<n;i++){
+    //     cout<<numbers[i];
+    // }
+    int target;
+    cin>>target;
+   
     for(int i=0;i<n;i++){
-        cin>>numbers[i];
+        if(numbers[i]+numbers[i+1]==target){
+            ans.push_back(i);
+            ans.push_back(i+1);
+        }
+      
     }
-    // int target;
-    // cin>>target;
-    int l=0;
-    int r=n-1;
-    int mid=l+(r-l)/2;
-    cout<<numbers[mid];
+   
+    for(int i=0;i<n;i++){
+        cout<<ans[i]<<" ";
+    }
+    
+    
+  
 
     return 0;
 
