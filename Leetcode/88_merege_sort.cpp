7eb036a6d22b34006a369fea8e6d0e7f8a91f229
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-11 23:54:37 +0530

    divya

--- Leetcode/88_merege_sort.cpp
+++ Leetcode/88_merege_sort.cpp
@@ -1,10 +1,22 @@
+
+/*
+Example 1:
+
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.*/
+
 #include<iostream>
 #include<vector>
 using namespace std;
 int main(){
-    vector<int> nums;
-    int m;
-    vector<int> nums2;
-    int n;
+    vector<int> nums={1,2,3,0,0,0};
+    int m=3;
+    vector<int> nums2={2,5,6};
+    int n=3;
     
+
+
 }
