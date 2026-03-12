d1da3c0f622ad265fc31eb27ae065d37e4da2b31
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-12 16:03:46 +0530

    divya

---
+++ Lecture 23/string.cpp
@@ -0,0 +1,22 @@
+/*
+character array:
+
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    //1st way
+    char a[]={'a','b','c'};
+
+
+    //2nd way
+    int n;
+    cin>>n;
+    char arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+
+    }
+
+}
