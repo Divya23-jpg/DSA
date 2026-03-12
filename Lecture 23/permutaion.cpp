6f5017ea78bd4fe526c4f28af6378cfa4dad78c4
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-12 15:46:40 +0530

    divya

--- Lecture 23/permutaion.cpp
+++ Lecture 23/permutaion.cpp
@@ -9,16 +9,31 @@ Lenography permutation : Dictionary order
 */
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 int main(){
 
     vector <int> nums={2,1,5,4,3,0,0};
+    next_permutation(nums.begin(),nums.end());// inbuilt function for permutation
     int n=nums.size();
     int index=-1;
-    for(int i=n-2;i<=0;i--){
+    for(int i=n-2;i>=0;i--){
         if(nums[i]<nums[i+1]){
             index=i;
             break;
         }
+
 }
+ if(index==-1){
+    reverse(nums.begin(),nums.end());
+    return;
+    }
+    for(int i=n-1;i>index;i--){
+        if(nums[i]>nums[index]){
+            swap(nums[i],nums[index]);
+            break;
+        }
+    }
+    sort(nums.begin()+index+1,nums.end());
 }
+
