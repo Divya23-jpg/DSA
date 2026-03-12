f6ace783af4f60d0b0bf8ab982c5895a96771f66
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-12 15:15:42 +0530

    divya

--- Lecture 23/permutaion.cpp
+++ Lecture 23/permutaion.cpp
@@ -7,4 +7,18 @@ total ways=m!
 
 Lenography permutation : Dictionary order
 */
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
 
+    vector <int> nums={2,1,5,4,3,0,0};
+    int n=nums.size();
+    int index=-1;
+    for(int i=n-2;i<=0;i--){
+        if(nums[i]<nums[i+1]){
+            index=i;
+            break;
+        }
+}
+}
