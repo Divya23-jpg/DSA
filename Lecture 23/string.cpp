5037a6d2871c1c9311ea565bdb0154786e17ce34
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-12 16:08:44 +0530

    divya

--- Lecture 23/string.cpp
+++ Lecture 23/string.cpp
@@ -4,19 +4,35 @@ character array:
 */
 
 #include<iostream>
+#include<vector>
 using namespace std;
 int main(){
     //1st way
     char a[]={'a','b','c'};
+    //size of an array
+    int m=sizeof(a)/sizeof(char);
+     //Traversing array
+    for(int i=0;i<m;i++){
+        cout<<a[i];
+
+    }
 
 
     //2nd way
     int n;
-    cin>>n;
+    cin>>n; //user input
     char arr[n];
     for(int i=0;i<n;i++){
         cin>>arr[i];
 
     }
+    //Traversing array
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+
+    }
+
+    //vector char
+    vector <char> b;
 
 }
