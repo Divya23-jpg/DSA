a523be00b3acad8940aee6c47eac6b45bdc252ae
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-14 00:05:03 +0530

    divya

--- Lecture 24/strings_2.cpp
+++ Lecture 24/strings_2.cpp
@@ -9,13 +9,24 @@ with whitespacing
 #include<vector>
 using  namespace std;
 int main(){
+    // string name={"Hello I am Divya"}; //string "  ", charachter ' '
 
-    string name={"Hello I am Divya"}; //string "  ", charachter ' '
+    // //traversal
+    // for(int i=0;i<name.size();i++){
+    //     cout<<name[i];
+    // }
 
-    //traversal
-    for(int i=0;i<name.size();i++){
-        cout<<name[i];
-    }
+
+
+    //input from user
+    string n;
+    cin>>n;
+    cout<<n;
+
+    //output with whitespace
+    string s;
+    getline(cin,s);
+    cout<<s<<" ";
     return 0;
 
     
