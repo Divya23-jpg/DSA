9a7fce29575fedfba0a678d53da1f21ef388e8dc
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-13 15:10:20 +0530

    divya

---
+++ Leetcode/3110_score_of_a_string.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string s="hello";
+    int sum=0;
+    for(int i=1;i<s.size();i++){
+        sum+=abs(s[i]-s[i-1]);
+    
+    }
+    return sum;
+}
+
