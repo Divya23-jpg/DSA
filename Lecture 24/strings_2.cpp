8ac40010bdad1ce4e9a11a1dbb871400f438efd0
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-13 14:19:40 +0530

    divya

--- Lecture 24/strings_2.cpp
+++ Lecture 24/strings_2.cpp
@@ -1,7 +1,20 @@
 /*
 strings: sequence of characteristics
 getline(cin,string_variable_name):sare string line stored in nums
+with whitespacing
 
 */
 
+#include<iostream>
+#include<vector>
+using  namespace std;
+int main(){
 
+    string name={"Divya"}; //string "  ", charachter ' '
+
+    //traversal
+    for(int i=0;i<name.size();i++){
+        cout<<name[i];
+    }
+    return 0;
+}
