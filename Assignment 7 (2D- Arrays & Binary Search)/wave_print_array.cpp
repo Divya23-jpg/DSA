2c71624f59123df7b5c28b5dbc9de7d4e816c00b
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-16 21:37:27 +0530

    divya

--- Assignment 7 (2D- Arrays & Binary Search)/wave_print_array.cpp
+++ Assignment 7 (2D- Arrays & Binary Search)/wave_print_array.cpp
@@ -1,3 +1,16 @@
+/*
+4 4
+11 12 13 14
+21 22 23 24
+31 32 33 34
+41 42 43 44
+
+11, 12, 13, 14, 24, 23, 22,
+21, 31, 32, 33, 34, 44, 43, 42, 41, END
+*/
+
+
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -14,12 +27,12 @@ int main(){
     }
     //output
     for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cout<<mat[j][i]<<" ";
+        for(int j=n-1;j>=0;j--){
+            cout<<mat[i][j]<<",";
         }
-        cout<<endl;
+        // cout<<endl;/
     }
-    cout<<endl;
+    cout<<"END";
    
   
 }
