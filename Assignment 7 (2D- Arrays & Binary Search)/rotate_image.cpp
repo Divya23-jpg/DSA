690af322dc4400fc4b4597949cf1569efe701d93
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-16 21:23:02 +0530

    divya

--- Assignment 7 (2D- Arrays & Binary Search)/rotate_image.cpp
+++ Assignment 7 (2D- Arrays & Binary Search)/rotate_image.cpp
@@ -2,22 +2,24 @@
 using namespace std;
 int main(){
     int n ;
-    int m;
-    cin>>n>>m;
-    int mat[n][m];
+    
+    cin>>n;
+    int mat[n][n];
    
     //input
     for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
+        for(int j=0;j<n;j++){
             cin>>mat[i][j];
         }
     }
     //output
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            cout<<mat[j][i];
+    for(int i=n-1;i>=0;i--){
+        for(int j=0;j<n;j++){
+            cout<<mat[j][i]<<" ";
         }
+        cout<<endl;
     }
+    cout<<endl;
    
   
 }
