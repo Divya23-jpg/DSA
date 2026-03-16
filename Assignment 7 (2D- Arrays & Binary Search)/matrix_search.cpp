422d3e0d8aa20bf05cbdc0b9284821b4bb75ca0b
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-16 21:00:20 +0530

    divya

---
+++ Assignment 7 (2D- Arrays & Binary Search)/matrix_search.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    int m;
+    cin>>n>>m;
+    int mat[n][m];
+   
+    //input
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>mat[i][j];
+        }
+    }
+    int key;
+    cin>>key;
+    int found=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(mat[i][j]==key){
+                found++;
+                
+            }
+            else{
+                found=0;
+            }
+        }
+    }
+    if(found==1){
+        cout<<"1";
+    }
+    else{
+         cout<<"0";
+
+    }
+    return 0;
+}
