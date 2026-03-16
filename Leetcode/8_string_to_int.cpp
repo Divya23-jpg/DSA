6ecacdb1ed339986c09a5e5df6023513ac2f8f0f
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-16 14:33:29 +0530

    aerdt

--- Leetcode/8_string_to_int.cpp
+++ Leetcode/8_string_to_int.cpp
@@ -1,11 +1,35 @@
-#include<iostream>
-#include<string>
-#include<algorithm>
+class Solution {
+public:
+    int myAtoi(string s) {
+    int i=0;
+    int n=s.size();
+      //white spaces
+    while(i<n and s[i]==' '){
+        i++;
+        // continue;
+    }
+    //sign 
+    int sign=1;
+    if(i<n and (s[i]=='+' or s[i]=='-')){
+        if(s[i]=='-'){
+            sign=-1;
+        }
+        i++;
+    }
+    //conversion
+    long long num=0;
+    while(i<n and isdigit(s[i])){
+        num=num*10+(s[i]-'0');
+        if(sign*num>INT_MAX){
+            return INT_MAX;
+        }
+        if(sign*num<INT_MIN){
+            return INT_MIN;
+        }
+        i++;
+    }
+    return sign*num;
 
-
-using namespace std;
-int main(){
-    string s="42";
-    
-  
-}
+        
+    }
+};
