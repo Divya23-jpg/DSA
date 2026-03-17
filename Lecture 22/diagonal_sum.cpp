49c7c98e99c9326efeaf2ae66621ff873da1ecad
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-17 18:39:23 +0530

    divya

---
+++ Lecture 22/diagonal_sum.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int diagonalSum(int mat[][100],int n){
+    int sum=0;
+    for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        if(i==j){
+            sum += mat[i][j];
+        }
+        else if(j==n-i-1){
+            sum += mat[i][j];
+        }
+    }
+}
+return sum;
+
+
+
+}
+int main(){
+   int n;
+   cin>>n;
+   int mat[n][n];
+   for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        cin>>mat[i][j];
+    }
+   }
+   for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        cout<<mat[i][j]<<" ";
+    }
+    cout<<endl;
+
+   }
+   cout<<diagonalSum(mat,n);
+   return 0;
+
+}
