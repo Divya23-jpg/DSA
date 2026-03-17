d94f2e3315594109d1ab9eabdb4e34b53db1ca01
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-17 20:16:56 +0530

    divya

--- Lecture 22/2 D Vector/search_in_2D_matrix.cpp
+++ Lecture 22/2 D Vector/search_in_2D_matrix.cpp
@@ -3,32 +3,49 @@
 10 11 16 20
 23 30 34 60 
 
+LEETCODE 74
+
 # Each row is sorted in non decresing order.
 meaning =<next value  
 # The first int of each row is greater tghan the last int
 of previous row
 
 */
-
-
-
 #include<iostream>
 #include<vector>
 using namespace std;
-int main(){
-    // int m,n;
-    // cin>>n>>m;
-    vector<vector <int> > mat={{1,2,3},{4,5,6},{7,8,9}};//3x3
-    
- 
 
-    
+bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int  i = 0, j = mat[0].size()-1;
+        while (i<mat.size() && j>=0){
+            if (mat[i][j] == target) return true;
+            else if (target>mat[i][j]) i+=1;
+            else if (target<mat[i][j]) j-= 1;
+
+
+        }
+        return false;
+    }
+
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+   vector<vector<int>> mat(n, vector<int>(m));
+
     for(int i=0;i<mat.size();i++){
         for(int j=0;j<mat[i].size();j++){
-            cout<<mat[i][j]<<" ";
+            cin>>mat[i][j];
         }
-        cout<<endl;
     }
+    int target;
+    cin>>target;
+     if (searchMatrix(mat, target))
+        cout << "Found\n";
+    else
+        cout << "Not Found\n";
+
   
+
     return 0;
 }
