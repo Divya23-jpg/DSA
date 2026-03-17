add1eb7b9ac3f30466dcfd9438e9860c4074f52c
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-17 18:59:39 +0530

    divya

--- Lecture 22/2 D Vector/2D_vector.cpp
+++ Lecture 22/2 D Vector/2D_vector.cpp
@@ -2,11 +2,19 @@
 #include<vector>
 using namespace std;
 int main(){
-    int m,n;
-    cin>>n>>m;
+    // int m,n;
+    // cin>>n>>m;
     vector<vector <int> > mat={{1,2,3},{4,5,6},{7,8,9}};//3x3
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
+    
+    /*
+    total rows=mat.size()
+
+    colums = ek row  me kitne elements h
+    total colums=mat[i].size() */ 
+
+    
+    for(int i=0;i<mat.size();i++){
+        for(int j=0;j<mat[i].size();j++){
             cout<<mat[i][j]<<" ";
         }
         cout<<endl;
