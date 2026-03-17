7a52567ecebc22a50fccb02ad24d616269ace9f5
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-17 18:55:33 +0530

    divya

--- Lecture 22/2 D Vector/2D_vector.cpp
+++ Lecture 22/2 D Vector/2D_vector.cpp
@@ -4,7 +4,13 @@ using namespace std;
 int main(){
     int m,n;
     cin>>n>>m;
-    vector  <vector <int> > is_array(n,vector<int> (m));
+    vector<vector <int> > mat={{1,2,3},{4,5,6},{7,8,9}};//3x3
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<mat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
   
     return 0;
 }
