89dd623b200696be558f7509a5757c7732c49974
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-17 11:28:46 +0530

    divya

---
+++ Lecture 22/maxSum_rowWise.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+
+void matrix(int mat[][100],int n,int m){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<mat[i][j]<<" ";
+        }
+    }
+}
+
+int main(){
+   
+    int n;
+    int m;
+    cin>>n>>m;
+    int mat[100][100];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>mat[i][j];
+        }
+    }
+    matrix(mat,n,m);
+    return 0;
+}
