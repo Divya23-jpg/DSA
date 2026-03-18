93c32ddec593a6eb9f4be8bbfb9b874380630fdc
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-18 13:46:41 +0530

    divya

--- Leetcode/Arrays or Vector/918_max_sum_circular.cpp
+++ Leetcode/Arrays or Vector/918_max_sum_circular.cpp
@@ -4,4 +4,25 @@ nhi ho k pehle element hota h
 
 
 */
+#include<iostream>
+using namespace std;
+
+  int total_sum = 0;
+        int curr_max = 0, max_sum = nums[0];
+        int curr_min = 0, min_sum = nums[0];
+
+        for (int x : nums) {
+            curr_max = max(x, curr_max + x);
+            max_sum = max(max_sum, curr_max);
+            curr_min = min(x, curr_min + x);
+            min_sum = min(min_sum, curr_min);
+
+            total_sum += x;
+        }
+        if (max_sum < 0) {
+            return max_sum;
+        }
+
+        return max(max_sum, total_sum - min_sum);
+    }
 
