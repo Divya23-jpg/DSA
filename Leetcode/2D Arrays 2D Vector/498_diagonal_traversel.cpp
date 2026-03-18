92e7b26edc8ee8ef4f60802c0d50029d0eca2bd5
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-18 14:23:07 +0530

    divya

--- Leetcode/2D Arrays 2D Vector/498_diagonal_traversel.cpp
+++ Leetcode/2D Arrays 2D Vector/498_diagonal_traversel.cpp
@@ -13,14 +13,79 @@ int main(){
         }
     }
     bool up=true;
-    int i=0;
+    int i=0,j=0;
     int r=0,c=0;
-    vector<int> ans(m*n);
-    while(r<n && c<m){
-        if(up){
-            while(r>0 and c<m-1){
-                
+    if (mat.empty() || mat[0].empty()) return {};
+    
+    int m = mat.size();
+    int n = mat[0].size();
+    vector<int> result;
+    
+    for (int s = 0; s <= m + n - 2; ++s) {
+        if (s % 2 == 0) {
+            int r = min(s, m - 1); 
+            int c = s - r;
+            while (r >= 0 && c < n) {
+                result.push_back(mat[r][c]);
+                r--;
+                c++;
+            }
+        } 
+        else {
+            int c = min(s, n - 1);
+            int r = s - c;
+            while (c >= 0 && r < m) {
+                result.push_back(mat[r][c]);
+                r++;
+                c--;
             }
         }
     }
-}
+     for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+             cout<<result[i][j];
+        }   
+    }
+// vector<int> ans(m*n);
+// while(r<n && c<m){
+//     if(up){
+//         while(r>0 and c<m-1){
+//             ans[i]=mat[r][c];
+//             i++;
+//             r--;
+//             c++;
+//         }
+//         ans[i]=mat[r][c];
+//         i++;
+//         if(c==m-1){
+//             r++;
+//         }
+       
+//         else{
+//             while(c>0 and r<n-1){
+//             ans[i]=mat[r][c];
+//             i++;
+//             r++;
+//             c--;
+//         }
+//         ans[i]=mat[r][c];
+//         i++;
+//         if(r==m-1){
+//             r++;
+//         }
+//         else{
+//             r++;
+//         }
+            
+//     }
+//     up=!up;
+
+
+//     }
+   
+    
+// }
+// for(int i=0;i<n;i++){
+// for(int j=0;j<m;j++){
+//     cout<<ans[i][j];
+// }
