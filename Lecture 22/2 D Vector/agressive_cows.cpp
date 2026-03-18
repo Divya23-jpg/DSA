31bd5b2da8ceb7ace190a40de3537405999d5f9a
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-18 20:01:58 +0530

    divya

---
+++ Lecture 22/2 D Vector/agressive_cows.cpp
@@ -0,0 +1,19 @@
+/*
+The first line contains two integers
+n (number of stalls) and c (number of cows).
+The second line contains n integers, 
+x1, x2, …, xn, representing the positions of the stalls.
+
+
+*/
+
+
+
+#include<iostream>
+#include<vector>
+using namespace std; 
+int main(){
+    int n,c;
+    cin>>n>>c;
+    
+}
