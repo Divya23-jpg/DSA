73528f4665e6ce799c3df7d397e19f7859be7d7a
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-19 11:03:50 +0530

    divya

--- Lecture 24/strings_2.cpp
+++ Lecture 24/strings_2.cpp
@@ -10,29 +10,30 @@ with whitespacing
 #include<string>
 using  namespace std;
 int main(){
+    //character strings
     char st[]={'a','b','c','\0'};//due to \0 it created string from char
     cout<<st; //string literal which value cant be change
     cout<<st[1]<<endl; //indexing starts from 0
     // if no charachter present then gives null
 
-    // string name={"Hello I am Divya"}; //string "  ", charachter ' '
+    string name={"Hello I am Divya"}; //string "  ", charachter ' '
 
-    // //traversal
-    // for(int i=0;i<name.size();i++){
-    //     cout<<name[i];
-    // }
+    //traversal
+    for(int i=0;i<name.size();i++){
+        cout<<name[i];
+    }
 
 
 
     //input from user
-    // string n;
-    // cin>>n;
-    // cout<<n;
+    string n;
+    cin>>n;
+    cout<<n;
 
-    // //output with whitespace
-    // string s;
-    // getline(cin,s);
-    // cout<<s<<" ";
+    //output with whitespace
+    string s;
+    getline(cin,s);
+    cout<<s<<" ";
     // return 0;
 
     
