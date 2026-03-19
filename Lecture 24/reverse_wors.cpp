c9bbb9c08b35865ea25c1cc7f22617a165409763
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-19 19:29:58 +0530

    divya

---
+++ Lecture 24/reverse_wors.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using  namespace std;
+
+
+
+string word_reverse(string s){
+    int n=s.length();
+    string ans="";
+    reverse(s.begin(),s.end());
+    for(int i=0;i<n;i++){
+        string word="";
+        while(i<n && s[i]!=' '){
+            word+=s[i];
+            i++;
+        }
+         reverse(word.begin(),word.end());
+         if(word.length()>0){
+            ans += " "+word;
+         }
+
+
+    }
+    return ans.substr(1);
+
+
+
+}
+int main(){
+    string s=" hello world ";
+    cout<<word_reverse(s);
+
+}
