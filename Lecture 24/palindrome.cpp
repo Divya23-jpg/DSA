f07e3bb0948f73645f012eac444d4ba1ef06cc9e
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-19 11:57:49 +0530

    divya

---
+++ Lecture 24/palindrome.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using  namespace std;
+
+
+
+bool palindrome(string s){
+    reverse(s.begin(),s.end());
+    
+
+}
+int main(){
+    string s="Madam";
+}
