ec8d44a6341568ebe7c010ca8cc7bb0654a34899
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-19 11:26:30 +0530

    divya

---
+++ Lecture 24/reverse_string.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using  namespace std;
+int main(){
+    // string str1="Divya";
+    // for(int i=str1.length()-1;i>=0;i--){
+    //     cout<<str1[i]<<" ";
+    // }
+
+    //reverse on char arr[]
+    // char s[]={'h','e','l','o'};
+    // int n=s.size();
+
+    // int st=0,e=n-1;
+
+
+
+    //reverse on string
+    string st="Divya";
+    reverse(st.begin(),st.end());  //return iterator
+    cout<<st<<endl;
+
+}
