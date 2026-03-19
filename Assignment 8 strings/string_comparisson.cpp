4865c1e61f976e0a864dd3e93fa4112620846597
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-19 20:11:12 +0530

    divya

---
+++ Assignment 8 strings/string_comparisson.cpp
@@ -0,0 +1,22 @@
+/*
+Sample Input
+aaabbccdsaa
+Sample Output
+a3b2c2d1s1a2
+
+*/
+
+#include<iostream>
+#include<string>
+#include <cctype>
+
+using namespace std;
+int main(){
+    string s="aaabbccdsaa";
+ 
+    for(int i=0;i<s.length();i++){
+        
+      
+    }
+    
+}
