c3799f5a76dbc58b1f163fd5dd4cd41c5165a51f
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-19 18:36:57 +0530

    divya

---
+++ Assignment 8 strings/can_u_read.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str="IAmACompetitiveProgrammer";
+    for(int i=0;i<str.length();i++){
+        // if(str[i]=="A"){
+
+        // }
+        cout<<str[3]<<endl;
+    }
+}
