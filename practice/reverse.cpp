01b21f71bdedf0393957a1474d9ce240baf3ba4c
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-20 15:27:06 +0530

    divya

---
+++ practice/reverse.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+//reverse by reverse loop
+void reverse(int a[],int n){
+
+    for(int i=n-1;i>=0;i--){
+        cout<<a[i]<<" ";
+    }
+    
+}
+
+//reverse by swaping
+void swap(int a[],int n){
+    int st=0,end=n-1;
+    while(st<end){
+        swap(a[st],a[end]);
+        st++;
+        end--;
+    }
+     // print after swapping
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+//    reverse(a,n);
+swap(a,n);
+
+  
+}
