930902e9f164809b9304273981b13b1e2e9b4aa9
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-21 13:52:09 +0530

    divya

--- practice/find_missing.cpp
+++ practice/find_missing.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 using namespace std;
+//one way
+int main() {
+    int arr[] = {1, 2, 4, 5}; // Missing 3
+    int n = 5; // supposed to be 1..5
+    int sum = 0;
+    for(int i=0; i<n-1; i++) sum += arr[i];
+    int total = n*(n+1)/2;
+    cout << "Missing element: " << total - sum;
+    return 0;
+}
 
+
+#include <iostream>
+using namespace std;
+
+//2nd way
 int main() {
     int arr[] = {1, 2, 3, 5};  // sequence with missing number
     int n = sizeof(arr) / sizeof(arr[0]);
+   
 
     // Formula: sum of first n natural numbers
     int total = (arr[n-1] * (arr[n-1] + 1)) / 2;
+   
 
     // Sum of given array
     int sum = 0;
