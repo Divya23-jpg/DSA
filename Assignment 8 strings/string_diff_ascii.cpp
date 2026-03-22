7a225116bbb9b0362e4328997e43c67fd18f9dc6
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-22 14:51:39 +0530

    divya

--- Assignment 8 strings/string_diff_ascii.cpp
+++ Assignment 8 strings/string_diff_ascii.cpp
@@ -13,10 +13,14 @@ using namespace std;
 
 int main(){
     string s;
-    int minus=0;
     cin>>s;
-    string word="";
-   
+    int dif=0;
+    int n=s.size();
+    for(int i=0;i<s.size()-1;i++){
+        dif=s[i+1]-s[i];
+        cout << s[i] << dif;  
+    }
+    cout<<s[n-1];
    
     return 0;
 }
