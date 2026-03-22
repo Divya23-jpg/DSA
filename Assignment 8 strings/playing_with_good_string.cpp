cf5a80321f453069791e619971df58bfb7f693d2
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-23 00:01:20 +0530

    divya

--- Assignment 8 strings/playing_with_good_string.cpp
+++ Assignment 8 strings/playing_with_good_string.cpp
@@ -5,19 +5,30 @@ Sample Output
 3
 */
 
-#include<iostream>
-#include<string>
-#include <cctype>
-
+#include <iostream>
+#include <string>
 using namespace std;
-int main(){
-    string s="cbaeicde";
-    int n=s.length();
-    
-    
 
+bool isVowel(char c) {
+    return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
+}
 
-   
-    
+int longestGoodSubstring(string s) {
+    int maxLen = 0, currLen = 0;
+    for (char c : s) {
+        if (isVowel(c)) {
+            currLen++;
+            maxLen = max(maxLen, currLen);
+        } else {
+            currLen = 0; // reset when consonant appears
+        }
+    }
+    return maxLen;
+}
 
+int main() {
+    string S;
+    cin >> S;
+    cout << longestGoodSubstring(S) << endl;
+    return 0;
 }
