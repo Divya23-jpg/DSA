5322637ce105bed06750ebcd89dee13b6c0c0dae
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-29 19:50:34 +0530

    divya

---
+++ Leetcode/Arrays or Vector/88_merge_sort.cpp
@@ -0,0 +1,23 @@
+/*
+Input: nums1 = [1,2,3,0,0,0], m = 3, 
+nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+*/
+#include<iostream>
+#include<vector>
+using namespace std;
+ void merge(vector<int>& A, int m, vector<int>& B, int n) {
+    int i=m-1; int j=n-1;
+        
+}
+int main(){
+    int m;
+    cin>>m;
+    vector<int> A(m);
+    int n;
+    cin>>n;
+    vector<int> B(n);
+    merge(A,m,B,n);
+    return 0;
+    
+}
