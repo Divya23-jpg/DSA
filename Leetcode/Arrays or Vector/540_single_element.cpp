b11a27e3704856aa3832d3377ac05f4073173d4d
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-29 19:23:59 +0530

    divya

--- Leetcode/Arrays or Vector/540_single_element.cpp
+++ Leetcode/Arrays or Vector/540_single_element.cpp
@@ -4,14 +4,17 @@ using namespace std;
 
 int singleNonDuplicate(vector<int>& nums) {
     int n=nums.size();
-    if(n==1){
-        return nums[0];
-    }
+    if(n==1) return nums[0];
+    
     int st=0;
-    int end=n-1; int mid=st+(end-st)/2;
+    int end=n-1; 
     
     while(st<=end){
-        if(nums[mid-1]!=nums[mid]!=nums[mid+1]){
+        int mid=st+(end-st)/2;
+        if(mid==0 && nums[0]!=nums[1]) return nums[mid];
+        if(mid==n-1 && nums[n-1]!=nums[n-2]) return nums[mid];
+
+        if(nums[mid-1]!=nums[mid] && nums[mid]!=nums[mid+1]){
             return nums[mid];
         }
         if(mid%2==0){
