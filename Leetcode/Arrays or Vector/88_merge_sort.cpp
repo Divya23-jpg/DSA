9715a3efc8ba473e37612b01340397dda327a4e8
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-03-29 20:27:37 +0530

    divya

--- Leetcode/Arrays or Vector/88_merge_sort.cpp
+++ Leetcode/Arrays or Vector/88_merge_sort.cpp
@@ -7,17 +7,39 @@ Output: [1,2,2,3,5,6]
 #include<vector>
 using namespace std;
  void merge(vector<int>& A, int m, vector<int>& B, int n) {
-    int i=m-1; int j=n-1;
-        
+    int i=m-1; int j=n-1; int k=m+n-1;
+    while(i>=0 && j>=0){
+        if(A[i]>=B[j]){
+            A[k]=A[i];
+            i--;
+            k--;
+        }
+        else{
+            A[k]=B[j];
+            j--;
+            k--;
+        }
+      
+    }
+    while(j>=0){
+        A[k]=B[j];
+        j--;
+        k--;
+    }
+  
+
+
 }
 int main(){
-    int m;
-    cin>>m;
-    vector<int> A(m);
-    int n;
-    cin>>n;
-    vector<int> B(n);
-    merge(A,m,B,n);
+    vector<int> A={ 1,2,3,0,0,0};
+    int m=3;
+    vector<int> B={2,5,6};
+    int n=3;
+   merge( A, m, B, n);
+   for(int i=0;i<m+n;i++){
+    cout<<A[i];
+   }
+ 
     return 0;
     
 }
