fbd8391adf3910a62ced8b1f8aed7d73468c4961
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-04-01 14:38:28 +0530

    divya

---
+++ Leetcode/Arrays or Vector/268_missing_numsber.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+ int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        n=n*(n+1)/2;
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            ans+=nums[i];
+        }
+        return n-ans;
+    }
+
+
+int main(){
+    vector<int> nums={0,1};
+    missingNumber(nums);
+
+}
