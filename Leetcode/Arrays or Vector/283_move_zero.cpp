6342355524cc9fd6cf99994e3105c5941cd960da
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-04-06 19:43:37 +0530

    divya

--- Leetcode/Arrays or Vector/283_move_zero.cpp
+++ Leetcode/Arrays or Vector/283_move_zero.cpp
@@ -18,8 +18,8 @@ Output: [1,3,12,0,0]
 using namespace std;
 int main(){
     vector <int> nums={0,1,0,3,12};
-     int n=nums.size();
-        int j=0;
+    int n=nums.size();
+    int j=0;
     
         for(int i=0;i<n;i++){
             if(nums[i]!=0){
