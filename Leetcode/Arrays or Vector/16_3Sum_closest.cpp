d8be8eafc5d419bc7e0bc9dda52930ef74129311
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-04-06 13:41:34 +0530

    divya

---
+++ Leetcode/Arrays or Vector/16_3Sum_closest.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int threeSumClosest(vector<int>& nums, int target) {
+    
+        
+}
+int main(){
+    vector <int> nums={1,2,1,-4};
+    int target=1;
+    threeSumClosest(nums,target);
+}
