a3b5dcc75ffada6477b5e794e94fa0a9aa653294
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-04-06 21:11:32 +0530

    divya

--- Assignment 6 Arrays/majority_element.cpp
+++ Assignment 6 Arrays/majority_element.cpp
@@ -18,20 +18,20 @@ int main(){
         cin>>a[i];
     }
     //optimal solution
-    // sort(a.begin(),a.end());
-    // int freq=1;int ans=a[0];
-    // for(int i=0;i<n;i++){
-    //     if(a[i]==a[i-1]){
-    //         freq++;
-    //     }
-    //     else{
-    //         freq=1;
-    //         ans=a[i];
-    //     }
-    //     if(freq>n/2){
-    //         cout<<ans;
-    //     }
-    // }
+    sort(a.begin(),a.end());
+    int freq=1;int ans=a[0];
+    for(int i=0;i<n;i++){
+        if(a[i]==a[i-1]){
+            freq++;
+        }
+        else{
+            freq=1;
+            ans=a[i];
+        }
+        if(freq>n/2){
+            cout<<ans;
+        }
+    }
 
     //morens or more optimal solution
     int freq=0,ans=0;
