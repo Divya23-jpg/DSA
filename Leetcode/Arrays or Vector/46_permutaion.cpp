97f6e430dad6303aa48b916a43b863f6154e8ed8
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-04-06 14:35:34 +0530

    divya

---
+++ Leetcode/Arrays or Vector/46_permutaion.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+ vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        ans.push_back(nums);
+        while(next_permutation(nums.begin(),nums.end())){
+            ans.push_back(nums);
+        }
+        return ans;
+    }
+
+
+int main(){
+    vector <int> nums={1,2,3,};
+    permute(nums);
+}
